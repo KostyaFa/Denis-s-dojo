871986664c1f51e0f82ac020d722adea6b24c3d8
KostyaFa <smirnovkn270895@yandex.com>
2023-02-14 23:35:39 +0300

    first_check

---
+++ APIs/firstapi.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    name : str
+
+dict_id = dict()
+
+@app.get('/user/{id}')
+async def read_item(id: int, item: User):
+    if dict_id.get(id) == None:
+        return f"Пользователь отсутсвует"
+    else: 
+        return dict_id.get(id).name
+
+@app.post('/user/{id}')
+async def create_item(id: int, item: User):
+    dict_id[id] = item
+    return f"Добавлен элемент {id}!"
+
+@app.put('/user/{id}')
+async def update_item(id: int, item: User):
+    if dict_id.get(id) == None:
+        return f'Обновлять нечего'
+    else: 
+        dict_id[id] = item
+        return f"Элемент {id} обновлен!"
+
+@app.delete('/user/{id}')
+async def delete_item(id: int, item: User):
+    if dict_id.get(id) == None:
+        return f'Удалять нечего'
+    else:
+        del dict_id[id]
+        return f"Элемент {id} удален!"
