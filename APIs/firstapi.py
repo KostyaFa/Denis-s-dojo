2d7d77f9f81de38c37a349d8c2f1489865746ad3
KostyaFa <119964499+KostyaFa@users.noreply.github.com>
2023-02-15 11:15:27 +0300

    Update firstapi.py

--- APIs/firstapi.py
+++ APIs/firstapi.py
@@ -1,25 +1,31 @@
+# импорт библиотек 
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+# создаем приложение
 app = FastAPI()
 
+# создаем User класс
 class User(BaseModel):
     name : str
-
+# словарь, для хранения User'ов
 dict_id = dict()
 
+# запрос get 
 @app.get('/user/{id}')
 async def read_item(id: int, item: User):
     if dict_id.get(id) == None:
         return f"Пользователь отсутсвует"
     else: 
         return dict_id.get(id).name
-
+# запрос post
 @app.post('/user/{id}')
 async def create_item(id: int, item: User):
     dict_id[id] = item
     return f"Добавлен элемент {id}!"
 
+# запрос put
 @app.put('/user/{id}')
 async def update_item(id: int, item: User):
     if dict_id.get(id) == None:
@@ -27,7 +33,7 @@ async def update_item(id: int, item: User):
     else: 
         dict_id[id] = item
         return f"Элемент {id} обновлен!"
-
+# запрос delete
 @app.delete('/user/{id}')
 async def delete_item(id: int, item: User):
     if dict_id.get(id) == None:
