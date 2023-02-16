3a6fef786c3300ff168939f42fef23ef4533261a
KostyaFa <smirnovkn270895@yandex.com>
2023-02-16 21:21:18 +0300

    second_check

--- APIs/firstapi.py
+++ APIs/firstapi.py
@@ -1,43 +1,35 @@
-# импорт библиотек 
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-# создаем приложение
 app = FastAPI()
 
-# создаем User класс
 class User(BaseModel):
     name : str
-# словарь, для хранения User'ов
+
 dict_id = dict()
 
-# запрос get 
 @app.get('/user/{id}')
 async def read_item(id: int, item: User):
     if dict_id.get(id) == None:
-        return f"Пользователь отсутсвует"
+        return 'Пользователь отсутсвует'
     else: 
         return dict_id.get(id).name
-# запрос post
+
 @app.post('/user/{id}')
 async def create_item(id: int, item: User):
     dict_id[id] = item
-    return f"Добавлен элемент {id}!"
+    return 'Добавлен элемент {id}!'
 
-# запрос put
 @app.put('/user/{id}')
 async def update_item(id: int, item: User):
-    if dict_id.get(id) == None:
-        return f'Обновлять нечего'
-    else: 
+    if dict_id.get(id) != None:
         dict_id[id] = item
-        return f"Элемент {id} обновлен!"
-# запрос delete
+        return 'Элемент {id} обновлен!'
+
 @app.delete('/user/{id}')
 async def delete_item(id: int, item: User):
     if dict_id.get(id) == None:
-        return f'Удалять нечего'
+        return 'Удалять нечего'
     else:
         del dict_id[id]
-        return f"Элемент {id} удален!"
+        return 'Элемент {id} удален!'
