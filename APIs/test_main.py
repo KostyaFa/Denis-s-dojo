f421c3fd748b267f965e58c12e98cb9b0aa753b7
Konstantin Smirnov <smirnovkn270895@yandex.com>
2023-03-28 00:38:27 +0300

    get cleaned up the api project

--- APIs/test_main.py
+++ APIs/test_main.py
@@ -1,42 +1,64 @@
 from fastapi.testclient import TestClient
-
 from firstapi import app
 
 client = TestClient(app)
 
+
 def test_create_item():
-    response = client.post(
-        '/user/3',
-        json = {'name': 'Den4ik'}                  
-        )
-    assert response.status_code == 200
-    assert 'Добавлен пользователь с id = 3'
+    responce = client.post('/user/3', json = {'name' : 'Den4ik'})
+    assert responce.json() == 'Добавлен пользователь с id = 3'
 
 def test_create_having_item():
-    response = client.post(
-        '/user/1',
-        json = {'name': 'Den4ik'}                  
-        )
-    assert response.status_code == 200
-    assert 'Пользователь с id = 1 уже существует'
-    
-def test_read_all():
-    responce = client.get(
-        '/user'
-        )
-    assert responce.status_code == 200
-    assert 'Пользователей нет'
+    responce = client.post('/user/3', json = {'name' : 'Ven4ik'})
+    assert responce.json() == 'Пользователь с id = 3 уже существует'
 
-def test_delete_all():
-    responce = client.delete(
-        '/user'
-        )
-    assert responce.status_code == 200
-    assert 'Пользователей нет'
-    
-# def test_read_one():
-#     responce = client.get(url="/user/1"    
-#         )
-#     assert responce.status_code == 200
-#     assert responce.json() == {'name': 'Den4ik'}
-    
+def test_get_exist_item():
+    responce = client.get('/user/3')
+    assert responce.json() == 'Den4ik'
+
+def test_get_no_exist_item():
+    responce = client.get('/user/4')
+    assert responce.json() == 'Пользователя с id = 4 не существует'
+
+def test_get_all():
+    responce = client.get('/user')
+    assert responce.json() == {'3' : {'name' : 'Den4ik'}}
+
+def test_put_new_item():
+    responce = client.put('/user/3', json={'name' : 'Ven4ik'})
+    assert responce.json() == 'Обновлены данные пользователя с id = 3'
+
+def test_put_old_item():
+    responce = client.put('/user/3', json={'name' : 'Ven4ik'})
+    assert responce.json() == 'Данные пользователя с id = 3 остались без изменений'
+
+def test_put_no_exist_item():
+    responce = client.put('/user/4', json={'name' : 'Ven4ik'})
+    assert responce.json() == 'Пользователя с id = 4 не существует'
+
+def test_del_item():
+    responce = client.delete('/user/3')
+    assert responce.json() == 'Пользователь с id = 3 удален'
+
+def test_del_no_exist_item():
+    responce = client.delete('/user/3')
+    assert responce.json() == 'Пользователя с id = 3 не существует'
+
+def test_get_all_no_exist():
+    responce = client.get('/user')
+    assert responce.json() == 'Пользователей нет'
+
+def test_del_all_no_exist():
+    responce = client.delete('/user')
+    assert responce.json() == 'Пользователей нет'
+
+def test_del_all():
+    client.post('/user/3', json = {'name' : 'Den4ik'})
+    client.post('/user/4', json = {'name' : 'Ven4ik'})
+
+    # responce = client.get('/user')
+    # assert responce.json() == {'3' : {'name' : 'Den4ik'}, '4' : {'name': 'Ven4ik'}}
+    #  >>> OK
+
+    responce = client.delete('/user')
+    assert responce.json() == 'Данные всех пользователей удалены'
